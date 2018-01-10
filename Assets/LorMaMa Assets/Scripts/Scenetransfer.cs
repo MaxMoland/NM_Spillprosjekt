79d8bd833c4ca7fe2541f522507fab189bd29d0f
kamizolo <kamizolo@hotmail.com>
2018-01-10 20:48:25 +0100

    Scene Transfer. Test funker mellom Movement Movement Test og Playable Test

---
+++ Assets/LorMaMa Assets/Scripts/Scenetransfer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenetransfer : MonoBehaviour {
+
+    public string scene;
+    public Color loadToColor = Color.white;
+    public GameObject _TriggerObj;
+
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, _TriggerObj.transform.position) < 0.8f)
+        {
+        Debug.Log("Trigger entered!");
+        //_DesiredObject.transform.position = transform.position;
+        //_DesiredObject.GetComponent<Pushable>().enabled = true;
+        Triggered();
+        }
+    }
+
+
+    public void Triggered()
+    {
+        Initiate.Fade(scene, loadToColor, 0.5f);
+    }
+}
