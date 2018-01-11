a36ed39e2195fac6e389d021fdc68c9e87edbfa5
Mntronslien <mntronslien@gmail.com>
2018-01-11 11:25:53 +0100

    sprite sort order

---
+++ Assets/LorMaMa Assets/Editor/SortSprites.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class SortSprites
+{
+    [MenuItem("LorMaMa/'Reorder Sprites in scene")]
+    private static void Reorder()
+    {
+        SpriteRenderer[] Sprites = GameObject.FindObjectsOfType<SpriteRenderer>();
+        foreach (var item in Sprites)
+        {
+            item.sortingOrder = (int)item.gameObject.transform.position.z * -1;
+        }
+    }
+}
---
+++ Assets/LorMaMa Assets/Scripts/SortOrderUpdater.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortOrderUpdater : MonoBehaviour {
+
+    private SpriteRenderer _Render;
+
+	// Use this for initialization
+	void Start () {
+        _Render = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _Render.sortingOrder = (int)gameObject.transform.position.z * -1;
+    }
+}
