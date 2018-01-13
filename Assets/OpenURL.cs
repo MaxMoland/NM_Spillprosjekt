c12dbe5fb5f6ebc00fa7184778c4d7522c1afacf
Mntronslien <mntronslien@gmail.com>
2018-01-13 04:24:08 +0100

    Final comit for NM...i think

---
+++ Assets/OpenURL.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour {
+    public string _URL = "http://www.mattiastronslien.com/TeamAvA.jpg";
+
+
+    public void Open()
+    {
+        Application.OpenURL(_URL);
+    }
+    public void Open(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
+
